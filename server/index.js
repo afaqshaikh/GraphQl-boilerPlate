64c3e2fa9935e8ddf9cf72f150b69b081a721556
afaqshaikh <zaidsmai920@hotmail.com>
2021-08-21 16:15:21 +0500

    Graphql boiler plate

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+let students = [
+    {
+        "id": 1,
+        "name": "Ali",
+        "email": "ali@gmail.com",
+        "age": 21
+    },
+    {
+        "id": 2,
+        "name": "Mohsin",
+        "email": "mohsin@gmail.com",
+        "age": 21
+    },
+    {
+        "id": 3,
+        "name": "Aamir",
+        "email": "aamir@gmail.com",
+        "age": 21
+    }
+]
+
+const resolvers = {
+    Query: {
+        students: () => students,
+    },
+    Mutation: {
+        addStudent: (e, { input }) => {
+            console.log(input)
+            students.push(
+                {
+                    name: input.name,
+                    age: input.age,
+                    email: input.email,
+                    id: input.id
+                }
+            )
+            return {
+                id: input.id,
+                name: input.name,
+                email: input.email,
+                age: input.age
+            }
+        }
+    }
+};
+const typeDefs = gql`
+    type Student {
+    id: Int
+    name: String
+    email: String
+    age: Int
+  }
+
+  input StdInput {
+    id: Int
+    name: String
+    email: String
+    age: Int
+  }
+
+  type Query {
+    students: [Student]
+  }
+
+  type Mutation {
+    addStudent(input: StdInput): Student
+  }
+
+  `
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
